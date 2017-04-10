85f60b1cf4e98d67c17ead69c4aaa84df6ad50e6
Jishnu Ramesh <Jishnu Ramesh>
2017-04-10 20:36:25 +0530

    Changed all logging in page.py

--- page.py
+++ page.py
@@ -3,13 +3,14 @@ from selenium.common.exceptions import NoAlertPresentException
 from Exceptions import NoElementAvailableException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from baselogging import *
 
 """
 ==============================================================================================================================
  This is the basic page which contains common modules for all pages. base page inherets from this page
 ==============================================================================================================================
 
-"""""
+
 #setting up logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,13 +19,14 @@ file_handler = logging.FileHandler('Selenium.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
+"""""
 
 class page (object):
     timeout_seconds = 20
     
     def __init__(self,driver):
         self.driver = driver
-        logger.info("driver intialized")
+        log.info("driver intialized")
         
     def find_element_by_locator(self,locator):
         return self.driver.find_element_by_locator(locator)
@@ -45,28 +47,28 @@ class page (object):
     #wait for element waits until the element is present on the screen or till time out is reached    
     def wait_for_element(self,locator):
         for i in range (self.timeout_seconds):
-            logger.info("inside the for loo[ for wait method in page module")
+            log.info("inside the for loop for wait method in page module")
             if self.driver.is_element_present(locator):
                 break
             time.sleep(.5)
         else:
                raise  NoElementAvailableException () 
-               logger.exception("Wait for element " + str(locator)+" timed out")
+               log.exception("Wait for element " + str(locator)+" timed out")
                return False 
         return True
     
     #wait for element waits until the element for mail is present on the screen or till time out is reached  
     def wait_for_mail(self,locator):
-        logger.info("executing to find if mail is available")
+        log.info("executing to find if mail is available")
         for i in range (self.timeout_seconds):
             if self.driver.is_element_available(locator):
-                logger.info("mail is found")
+                log.info("mail is found")
                 break
-            logger.info("sleeping to find the mail identified by locator =  " + str(locator))
+            log.info("sleeping to find the mail identified by locator =  " + str(locator))
             time.sleep(.5)
         else:
                raise  NoElementAvailableException () 
-               logger.exception("NO Build Today... Exiting")
+               log.exception("NO Build Today... Exiting")
                return False
         return True
     
@@ -74,33 +76,33 @@ class page (object):
     def wait_for_hidden(self,locator):
         for i in range (self.timeout_seconds):
             if self.driver.is_element_present(locator):
-                logger.info("sleeping to find the element identified by locator = " + str(locator))
+                log.info("sleeping to find the element identified by locator = " + str(locator))
                 time.sleep(.5)
             else:
                 break
             
         else :
             raise NoElementAvailableException ()
-            logger.exception("Wait for the hidden element timed out")
+            log.exception("Wait for the hidden element timed out")
         return True
     
     def Accept_alert(self):
         for i in range (self.timeout_seconds):
-            logger.debug("before_try_")
+            log.debug("before_try_")
             try:
-                logger.debug("Inside try wait alert")
+                log.debug("Inside try wait alert")
                 jalert = self.driver.switch_to.alert
-                logger.info("switched to alert")
+                log.info("switched to alert")
                 jalert.accept()
-                logger.info("alert accepted")
+                log.info("alert accepted")
                 return 
             except NoAlertPresentException as nape:
-                logger.warning("alert not seen , sleeping till alert is shown")
+                log.warning("alert not seen , sleeping till alert is shown")
             time.sleep(.5)
         else:
-            logger.debug("inside else")
+            log.debug("inside else")
             raise NoAlertPresentException ()
-        logger.exception("no alert observed , returing")
+        log.exception("no alert observed , returing")
         return True
     
     #used to navigate from the current window to newly created window.Not used in this example
@@ -121,10 +123,10 @@ class page (object):
             window_handles_names = self.driver.window_handles
             if len(window_handles_names) == 1:
                 self.driver.switch_to_window(window_handles_names[0])
-                logger.info("mail sent confirmed")
+                log.info("mail sent confirmed")
                 return 
             else:
-                logger.info("waiting for mail to be sent")
+                log.info("waiting for mail to be sent")
                 time.sleep(.5)
             
     
