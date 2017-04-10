d8546055aa30eb11d96c4fd9500a293c39dc10ca
Jishnu Ramesh <Jishnu Ramesh>
2017-04-10 20:29:18 +0530

    Changed all logging in webelement.py adn timecheck.py

--- timecheck.py
+++ timecheck.py
@@ -1,6 +1,6 @@
 import time,logging
 from datetime import date, timedelta
-
+from baselogging import *
 
 """
 ==============================================================================================================================
@@ -8,7 +8,7 @@ from datetime import date, timedelta
  received from yesterday through today
 =============================================================================================================================
 
-"""""
+
 #setting up logger
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
@@ -16,7 +16,7 @@ formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 file_handler = logging.FileHandler('Selenium.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
-
+"""""
 #not used in the current example. Used to get out the specific time for filtering emails. 
 class Find_Time():
     def Get_Search_Days(self):
@@ -26,5 +26,5 @@ class Find_Time():
         Yester_Day = Yesterday.strftime("%B %d")
         #print Yester_Day
         search_input = str(Yester_Day)+ " .. " + str(Current_Day)
-        logger.info("returning the search to be inputted as " + str(search_input))
+        log.info("returning the search to be inputted as " + str(search_input))
         return search_input
