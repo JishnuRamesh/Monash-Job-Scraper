5715ad29f8793c243057c357b92d2055222e6ee8
Jishnu Ramesh <Jishnu Ramesh>
2017-04-10 20:31:12 +0530

    Changed all logging in Readconfig.py

--- Readconfig_file.py
+++ Readconfig_file.py
@@ -1,12 +1,13 @@
 from backports import configparser
 import logging
+from baselogging import *
 
 """
 ==============================================================================================================================
  This file reads the concerned config file and inputs it into a dictionary which is then returned to base page
 ==============================================================================================================================
 
-"""""
+
 
 #setting up logger
 logger = logging.getLogger(__name__)
@@ -15,21 +16,22 @@ formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 file_handler = logging.FileHandler('Selenium.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
+""""" 
 
 def get_values_from_configfile(index):
     cfg = configparser.ConfigParser()
     cfg.read('config_file.ini')
-    logger.info("Reading configuration file into configparser")
+    log.info("Reading configuration file into configparser")
 
     dic = {}
     sec = cfg.sections()
 
     for items in cfg[sec[index]]:
         key = items
-        logger.info("imported  key  " + str(key) + "  to dictionary")
+        log.info("imported  key  " + str(key) + "  to dictionary")
         value = cfg[sec[index]][items]
         value = value.split("'")[1]
-        logger.info("imported value  " + str(value) + "  to dictionary")
+        log.info("imported value  " + str(value) + "  to dictionary")
         dic[key] = value
-    logger.info ("returning the created dictionary of config file values to base file")
+    log.info ("returning the created dictionary of config file values to base file")
     return dic
