9a131523cdfbc48a5fca582c5234691f97e44a34
Jishnu Ramesh <Jishnu Ramesh>
2017-04-10 18:24:26 +0530

    removed unwanted configparser line from base.py

--- base.py
+++ base.py
@@ -3,7 +3,7 @@ from page import page
 from timecheck import Find_Time
 import timecheck,logging
 import time
-from ConfigParser import ConfigParser
+#from ConfigParser import ConfigParser
 import Readconfig_file
 
 
